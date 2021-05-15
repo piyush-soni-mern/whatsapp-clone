525ab21490ae5c586253ff7bfe42f0d4af4f9317
SONI <sonipiyush993@gmail.com>
2021-05-15 12:38:54 +0530

    Changed whatsapp-frontend

--- whatsapp-frontend/src/axios.js
+++ whatsapp-frontend/src/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const development = "http://localhost:5000";
+const production = "https://mern-whastapp.herokuapp.com/";
+
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.NODE_ENV ? production : development,
 });
 
 export default instance;
